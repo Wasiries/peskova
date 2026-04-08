3638d081275c1074ce8a1ded6ec4c95cf7434d73
Wasiries <wasiries.email@yandex.ru>
2026-04-08 22:04:26 +0300

    newton method solved

---
+++ newton_method/src/main.rs
@@ -0,0 +1,30 @@
+fn f(x: f64) -> f64 {
+    return x.powf(4.0) + 8.0 * x.powf(2.0) + 8.0;
+}
+
+fn df(x: f64) -> f64 {
+    return 4.0 * x.powf(3.0) + 16.0 * x;
+}
+
+
+fn main() {
+    let epsilon = 1e-8;
+    let max_iter: usize = 1000;
+
+    let mut x_old: f64 = 0.0;
+
+    for _ in 0..max_iter {
+        let derivative = df(x_old); 
+        if derivative.abs() < 1e-10 {
+            break;
+        }
+        let x_new = x_old - f(x_old) / derivative;
+
+        if (x_new - x_old).abs() < epsilon {
+            break;
+        }
+        x_old = x_new;
+    }
+
+    println!("approximation to solution: {}", x_old);
+}
